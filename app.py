31c4018c32c59feb8dccc1f68579b750cd5cf690
Riviane Maria <133921292+Rivianee@users.noreply.github.com>
2024-07-23 11:27:46 -0300

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@
     "colab": {
       "provenance": [],
       "authorship_tag": "ABX9TyOGoj3RBkAEg32gF7/LIjcm",
-      "include_colab_link": true
+      "include_colab_link": True
     },
     "kernelspec": {
       "name": "python3",
